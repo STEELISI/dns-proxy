64a05f6c9a353eaa0881cf5037402395b4b57044
Haoda Wang <harry@h313.info>
2020-02-25 17:19:40 -0800

    add get_domain_name and insert_tlds

--- dns.h
+++ dns.h
@@ -2,22 +2,39 @@
 #define DNS_PROXY__DNS_H
 
 #include <string>
-#include <fstream>
-#include <unordered_set>
-
-// Check whether a packet is a standard QUERY
+/**
+ * Check whether a packet is a standard QUERY
+ * @param buffer the input packet as a byte array
+ * @return true if the request is a query
+ */
 bool check_if_query(const uint8_t *buffer);
 
-// Check if a packet is going to have an invalid TLD
+
+/**
+ * Check if a packet is has an invalid TLD
+ * @param buffer the input packet as a byte array
+ * @return true if the TLD is valid
+ */
 bool check_if_tld_valid(const uint8_t *buffer);
 
-// Get a domain name from a packet
+/**
+ * Get the domain name from a packet
+ * @param buffer the input packet as a byte array
+ * @return the domain name as a string
+ */
 std::string get_domain_name(const uint8_t *buffer);
 
-// Read valid TLDs from file into valid_tlds
-void insert_tlds(std::string input_file);
-
-// Create an NXDOMAIN reply to the packet in the buffer
+/**
+ * Create an NXDOMAIN reply to the packet in the buffer
+ * @param buffer the input packet as a byte array
+ * @return a packet as a byte array
+ */
 uint8_t **create_nxdomain_reply(const uint8_t *buffer);
+
+/**
+ * Read each line of a input file for TLDs
+ * @param input_file location of input tld
+ */
+void insert_tlds(const std::string& input_file);
 
 #endif //DNS_PROXY__DNS_H
