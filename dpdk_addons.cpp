b3b4828dff65117623ab21c17f519f593b456a5e
Haoda Wang <haodawa@isi.edu>
2020-02-28 16:21:15 -0800

    fix typo

--- dpdk_addons.cpp
+++ dpdk_addons.cpp
@@ -11,9 +11,7 @@ void init_port(unsigned int port, rte_mempool *pktmbuf_pool) {
   struct rte_eth_dev_info dev_info;
   int ret = rte_eth_dev_info_get(port, &dev_info);
   if (ret != 0)
-    rte_exit(EXIT_FAILURE,
-             "Error during getting device (port %u) info: %s\n",
-             port,
+    rte_exit(EXIT_FAILURE, "Error during getting device (port %u) info: %s\n", port,
              strerror(-ret));
 
   struct rte_eth_conf local_port_conf = port_conf;
@@ -33,15 +31,17 @@ void init_port(unsigned int port, rte_mempool *pktmbuf_pool) {
   rxq_conf.offloads = local_port_conf.rxmode.offloads;
   ret =
       rte_eth_rx_queue_setup(port, 0, nb_rxd, rte_eth_dev_socket_id(port), &rxq_conf,
-          pktmbuf_pool);
+                             pktmbuf_pool);
   if (ret < 0)
-    rte_exit(EXIT_FAILURE, "Could not setup up RX queue for port%u (%d)\n", (unsigned) port, ret);
+    rte_exit(EXIT_FAILURE, "Could not setup up RX queue for port%u (%d)\n", (unsigned) port,
+             ret);
 
   struct rte_eth_txconf txq_conf = dev_info.default_txconf;
   txq_conf.offloads = local_port_conf.txmode.offloads;
   ret = rte_eth_tx_queue_setup(port, 0, nb_txd, rte_eth_dev_socket_id(port), &txq_conf);
   if (ret < 0)
-    rte_exit(EXIT_FAILURE, "Could not setup up TX queue for port%u (%d)\n", (unsigned) port, ret);
+    rte_exit(EXIT_FAILURE, "Could not setup up TX queue for port%u (%d)\n", (unsigned) port,
+             ret);
 
   // Start the port
   ret = rte_eth_dev_start(port);
@@ -49,3 +49,24 @@ void init_port(unsigned int port, rte_mempool *pktmbuf_pool) {
     rte_exit(EXIT_FAILURE, "Could not start port%u (%d)\n", (unsigned) port, ret);
 }
 
+void kni_egress(uint16_t port_id, rte_kni *kni_port)
+{
+  unsigned nb_tx, num;
+  uint32_t nb_kni;
+  struct rte_mbuf *pkts_burst[PKT_BURST_SZ];
+
+  // Burst rx from kni
+  num = rte_kni_rx_burst(kni_port, pkts_burst, PKT_BURST_SZ);
+  if (unlikely(num > PKT_BURST_SZ)) {
+    RTE_LOG(ERR, APP, "Error receiving from KNI\n");
+    return;
+  }
+
+  // Burst tx to eth
+  nb_tx = rte_eth_tx_burst(port_id, 0, pkts_burst, (uint16_t)num);
+
+  // Free mbufs not transferred to NIC
+  if (unlikely(nb_tx < num))
+    for(unsigned pkt_it = 0; pkt_it < num - nb_tx; pkt_it++)
+      rte_pktmbuf_free(pkts_burst[pkt_it]);
+}
