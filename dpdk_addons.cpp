5c20557ac88717840ec8291e0f236b1b1e61cef4
Haoda Wang <haodawa@isi.edu>
2020-03-12 13:41:49 -0700

    clang format everything

--- dpdk_addons.cpp
+++ dpdk_addons.cpp
@@ -5,14 +5,14 @@ void init_port(unsigned int port, rte_mempool *pktmbuf_pool) {
   uint16_t nb_txd = NB_TXD;
 
   // Initialise device and RX/TX queues
-  RTE_LOG(INFO, APP, "Initialising port %u ...\n", (unsigned) port);
+  RTE_LOG(INFO, APP, "Initialising port %u ...\n", (unsigned)port);
   fflush(stdout);
 
   struct rte_eth_dev_info dev_info;
   int ret = rte_eth_dev_info_get(port, &dev_info);
   if (ret != 0)
-    rte_exit(EXIT_FAILURE, "Error during getting device (port %u) info: %s\n", port,
-             strerror(-ret));
+    rte_exit(EXIT_FAILURE, "Error during getting device (port %u) info: %s\n",
+             port, strerror(-ret));
 
   struct rte_eth_conf local_port_conf = port_conf;
   if (dev_info.tx_offload_capa & DEV_TX_OFFLOAD_MBUF_FAST_FREE)
@@ -20,36 +20,40 @@ void init_port(unsigned int port, rte_mempool *pktmbuf_pool) {
 
   ret = rte_eth_dev_configure(port, 1, 1, &local_port_conf);
   if (ret < 0)
-    rte_exit(EXIT_FAILURE, "Could not configure port%u (%d)\n", (unsigned) port, ret);
+    rte_exit(EXIT_FAILURE, "Could not configure port%u (%d)\n", (unsigned)port,
+             ret);
 
   ret = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
   if (ret < 0)
-    rte_exit(EXIT_FAILURE, "Could not adjust number of descriptors for port%u (%d)\n",
-             (unsigned) port, ret);
+    rte_exit(EXIT_FAILURE,
+             "Could not adjust number of descriptors for port%u (%d)\n",
+             (unsigned)port, ret);
 
   struct rte_eth_rxconf rxq_conf = dev_info.default_rxconf;
   rxq_conf.offloads = local_port_conf.rxmode.offloads;
-  ret =
-      rte_eth_rx_queue_setup(port, 0, nb_rxd, rte_eth_dev_socket_id(port), &rxq_conf,
-                             pktmbuf_pool);
+  ret = rte_eth_rx_queue_setup(port, 0, nb_rxd, rte_eth_dev_socket_id(port),
+                               &rxq_conf, pktmbuf_pool);
   if (ret < 0)
-    rte_exit(EXIT_FAILURE, "Could not setup up RX queue for port%u (%d)\n", (unsigned) port,
-             ret);
+    rte_exit(EXIT_FAILURE, "Could not setup up RX queue for port%u (%d)\n",
+             (unsigned)port, ret);
 
   struct rte_eth_txconf txq_conf = dev_info.default_txconf;
   txq_conf.offloads = local_port_conf.txmode.offloads;
-  ret = rte_eth_tx_queue_setup(port, 0, nb_txd, rte_eth_dev_socket_id(port), &txq_conf);
+  ret = rte_eth_tx_queue_setup(port, 0, nb_txd, rte_eth_dev_socket_id(port),
+                               &txq_conf);
   if (ret < 0)
-    rte_exit(EXIT_FAILURE, "Could not setup up TX queue for port%u (%d)\n", (unsigned) port,
-             ret);
+    rte_exit(EXIT_FAILURE, "Could not setup up TX queue for port%u (%d)\n",
+             (unsigned)port, ret);
 
   // Start the port
   ret = rte_eth_dev_start(port);
   if (ret < 0)
-    rte_exit(EXIT_FAILURE, "Could not start port%u (%d)\n", (unsigned) port, ret);
+    rte_exit(EXIT_FAILURE, "Could not start port%u (%d)\n", (unsigned)port,
+             ret);
 }
 
-unsigned int kni_change_mtu(uint16_t port, unsigned int new_mtu, rte_mempool *pktmbuf_pool) {
+unsigned int kni_change_mtu(uint16_t port, unsigned int new_mtu,
+                            rte_mempool *pktmbuf_pool) {
   uint16_t nb_rxd = 1024;
   rte_eth_conf conf;
   rte_eth_dev_info dev_info;
@@ -66,7 +70,8 @@ unsigned int kni_change_mtu(uint16_t port, unsigned int new_mtu, rte_mempool *pk
     conf.rxmode.offloads &= ~DEV_RX_OFFLOAD_JUMBO_FRAME;
 
   // Set up max packet length including headers
-  conf.rxmode.max_rx_pkt_len = new_mtu + KNI_ENET_HEADER_SIZE + KNI_ENET_FCS_SIZE;
+  conf.rxmode.max_rx_pkt_len =
+      new_mtu + KNI_ENET_HEADER_SIZE + KNI_ENET_FCS_SIZE;
 
   // Reconfigure port and increase descriptors to 1024
   rte_eth_dev_configure(port, 1, 1, &conf);
@@ -75,13 +80,15 @@ unsigned int kni_change_mtu(uint16_t port, unsigned int new_mtu, rte_mempool *pk
   // Setup RX queue
   rxq_conf = dev_info.default_rxconf;
   rxq_conf.offloads = conf.rxmode.offloads;
-  rte_eth_rx_queue_setup(port, 0, nb_rxd, rte_eth_dev_socket_id(port), &rxq_conf, pktmbuf_pool);
+  rte_eth_rx_queue_setup(port, 0, nb_rxd, rte_eth_dev_socket_id(port),
+                         &rxq_conf, pktmbuf_pool);
 
   // Start the port
   rte_eth_dev_start(port);
 }
 
-rte_kni *init_kni(unsigned int port, uint8_t mac_addr[], rte_mempool *pktmbuf_pool) {
+rte_kni *init_kni(unsigned int port, uint8_t mac_addr[],
+                  rte_mempool *pktmbuf_pool) {
   rte_kni_conf conf;
   rte_kni_ops ops;
   rte_eth_dev_info dev_info;
@@ -100,15 +107,17 @@ rte_kni *init_kni(unsigned int port, uint8_t mac_addr[], rte_mempool *pktmbuf_po
 
   // Set stuff up
   ops.port_id = port;
-  ops.change_mtu = reinterpret_cast<int (*)(uint16_t, unsigned int)>(kni_change_mtu(port, 1500, pktmbuf_pool));
-  ops.config_network_if = reinterpret_cast<int (*)(uint16_t, uint8_t)>(rte_eth_dev_start(port));
-  ops.config_mac_address = reinterpret_cast<int (*)(uint16_t, uint8_t *)>(rte_eth_dev_default_mac_addr_set(port,
-                                                                                                           (struct rte_ether_addr *) mac_addr));
+  ops.change_mtu = reinterpret_cast<int (*)(uint16_t, unsigned int)>(
+      kni_change_mtu(port, 1500, pktmbuf_pool));
+  ops.config_network_if =
+      reinterpret_cast<int (*)(uint16_t, uint8_t)>(rte_eth_dev_start(port));
+  ops.config_mac_address = reinterpret_cast<int (*)(uint16_t, uint8_t *)>(
+      rte_eth_dev_default_mac_addr_set(port,
+                                       (struct rte_ether_addr *)mac_addr));
   return rte_kni_alloc(pktmbuf_pool, &conf, &ops);
 }
 
-void kni_egress(uint16_t port_id, rte_kni *kni_port)
-{
+void kni_egress(uint16_t port_id, rte_kni *kni_port) {
   unsigned nb_tx, num;
   struct rte_mbuf *pkts_burst[PKT_BURST_SZ];
 
@@ -124,6 +133,6 @@ void kni_egress(uint16_t port_id, rte_kni *kni_port)
 
   // Free mbufs not transferred to NIC
   if (unlikely(nb_tx < num))
-    for(unsigned pkt_it = 0; pkt_it < num - nb_tx; pkt_it++)
+    for (unsigned pkt_it = 0; pkt_it < num - nb_tx; pkt_it++)
       rte_pktmbuf_free(pkts_burst[pkt_it]);
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <signal.h>
 
-
 #include "dns.h"
 #include "dpdk_addons.h"
 
@@ -35,8 +34,8 @@ int main(int argc, char *argv[]) {
     rte_exit(EXIT_FAILURE, "Could not initialise EAL (%d)\n", ret);
 
   // Create mubf pool
-  pktmbuf_pool = rte_pktmbuf_pool_create("mbuf_pool", 131072,
-                                         32, 0, 4096, rte_socket_id());
+  pktmbuf_pool = rte_pktmbuf_pool_create("mbuf_pool", 131072, 32, 0, 4096,
+                                         rte_socket_id());
   if (pktmbuf_pool == nullptr)
     rte_exit(EXIT_FAILURE, "Could not initialise mbuf pool\n");
 
