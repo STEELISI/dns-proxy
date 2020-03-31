3e73cb89c8a585087e2009ca4decbfb97f43dced
Haoda Wang <haodawa@isi.edu>
2020-03-30 21:18:45 -0700

    switch main code over to C and base it off kni.c example

--- dns.h
+++ dns.h
@@ -5,36 +5,38 @@
 /**
  * Check whether a packet is a standard QUERY
  * @param buffer the input packet as a byte array
- * @return true if the request is a query
+ * @return 1 if the request is a query, 0 otherwise
  */
-bool check_if_query(const uint8_t *buffer);
+extern "C" int check_if_query(const uint8_t *buffer);
 
 
 /**
  * Check if a packet is has an invalid TLD
  * @param buffer the input packet as a byte array
- * @return true if the TLD is valid
+ * @return 1 if the TLD is valid, 0 otherwise
  */
-bool check_if_tld_valid(const uint8_t *buffer);
+extern "C" int check_if_tld_valid(const uint8_t *buffer);
 
 /**
  * Get the domain name from a packet
  * @param buffer the input packet as a byte array
- * @return the domain name as a string
+ * @return the domain name as a cstring
  */
-std::string get_domain_name(const uint8_t *buffer);
+extern "C" char* get_domain_name(const uint8_t *buffer);
+
+
 
 /**
  * Create an NXDOMAIN reply to the packet in the buffer
  * @param buffer the input packet as a byte array
  * @return a packet as a byte array
  */
-uint8_t **create_nxdomain_reply(const uint8_t *buffer);
+extern "C" uint8_t **create_nxdomain_reply(const uint8_t *buffer);
 
 /**
  * Read each line of a input file for TLDs
  * @param input_file location of input tld
  */
-void insert_tlds(const std::string& input_file);
+extern "C" void insert_tlds(const std::string& input_file);
 
 #endif //DNS_PROXY__DNS_H
