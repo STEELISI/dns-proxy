184442a670202420e8097a318afeae322cb7e689
Haoda Wang <harry@h313.info>
2020-04-07 19:33:56 -0400

    Finish up DPDK side for dropping invalid TLDs

--- dns.cpp
+++ dns.cpp
@@ -3,17 +3,16 @@
 #include <algorithm>
 #include <cstring>
 #include <fstream>
-#include <string>
 #include <unordered_set>
 #include <rte_malloc.h>
 #include <rte_memcpy.h>
 
-std::unordered_set<std::string> *valid_tlds = new std::unordered_set<std::string>;
+std::unordered_set<std::string> valid_tlds;
 
-int request_is_query(const unsigned char *buffer) {
+bool check_if_query(const unsigned char *buffer) {
     // Get the second set of 16 bits
     uint16_t first_16_bits = 0;
-    memcpy(&first_16_bits, buffer + 2, 2);
+    rte_memcpy(&first_16_bits, buffer + 2, 2);
 
     // Read byte 16 and make sure it's a query first
     if ((first_16_bits & 0x8000) != 0x0000)
@@ -35,7 +34,7 @@ void insert_tlds(const std::string& input_file) {
             valid_tlds->insert(in_str);
 }
 
-unsigned char* get_domain_name(const unsigned char *buffer) {
+std::string get_domain_name(const unsigned char *buffer) {
     std::string domain;
     // Skip header
     int i = 12;
@@ -45,14 +44,11 @@ unsigned char* get_domain_name(const unsigned char *buffer) {
         i++;
     }
 
-    // We want to use DPDK's cstring stuff for minimum latency
-    unsigned char* domain_ret = static_cast<unsigned char *>(rte_malloc("string", domain.size(), 0));
-    rte_memcpy(domain_ret, domain.c_str(), domain.size());
-    return domain_ret;
+    return domain;
 }
 
 int check_if_tld_valid(const unsigned char *buffer) {
-    std::string domain = reinterpret_cast<const char *>(get_domain_name(buffer));
+    std::string domain = get_domain_name(buffer);
 
     // Get TLD and make it uppercase
     std::string tld = domain.substr(domain.find_last_of('.') + 1, domain.length() - 1);
@@ -69,4 +65,8 @@ unsigned char **create_nxdomain_reply(const unsigned char *buffer) {
 
 
     return &ret_packet;
+}
+
+bool tld_setup() {
+
 }
